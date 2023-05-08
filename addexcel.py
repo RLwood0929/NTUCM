e3f435af8d871ea605101b48ee94a994211425aa
Qian <wood123487@gmail.com>
2023-05-08 22:59:37 +0800

    addexcel.py模組化，將上傳檔案與執行串聯

--- addexcel.py
+++ addexcel.py
@@ -14,18 +14,20 @@ Created on Fri May 5 19:56:02 2023
 import pandas as pd
 import json
 
-df = pd.read_excel("data.xlsx")
-x, y = df.shape
-df.insert(2, column = "總和", value = "0")
+def add0(filename):
+    df = pd.read_excel(filename)
+    x, y = df.shape
+    df.insert(2, column = "總和", value = "0")
 
-for i in range(x):
-    add = df.iloc[i, 0]
-    summand = df.iloc[i, 1]
-    ans = add + summand
-    df.iloc[i, 2] = ans
+    for i in range(x):
+        add = df.iloc[i, 0]
+        summand = df.iloc[i, 1]
+        ans = add + summand
+        df.iloc[i, 2] = ans
 
-df.to_excel("結果.xlsx", index = None, header = True)
-df2 = df.to_json(orient = "records", force_ascii= False)
+    df.to_excel("結果.xlsx", index = None, header = True)
+    df2 = df.to_json(orient = "records", force_ascii= False)
 
-with open("結果.json", "w") as file:
-    json.dump(df2, file, indent = 4, ensure_ascii = False)
+    with open("結果.json", "w") as file:
+        json.dump(df2, file, indent = 4, ensure_ascii = False)
+    return "done"
